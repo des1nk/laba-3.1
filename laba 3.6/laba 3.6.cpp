dce90d23626d1ccb7b1bea6198e400b61cbbc66a
kosmo1337 <54933914+kosmo1337@users.noreply.github.com>
2019-09-29 13:40:57 +0300

    Add files via upload

---
+++ laba 3.6/laba 3.6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+int main()
+{
+	float A, x;
+	cout << "A=";
+	cin >> A;
+	A = A * A;
+	x = A * A;
+	A = x * x;
+	cout << "----\nA=" << A;
+}
---
+++ laba 3.7/laba 3.7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A, x, y;
+	cout << "A=";
+	cin >> A;
+	x = A * A;
+	y = A * x;
+	A = x * y;
+	x = A * A;
+	A = A * x;
+	cout << "----\nA=" << A;
+}
